0bcec416682b1a23b1d4bbde60b6565d1d9ac755
rdoochin <42946090+rdoochin@users.noreply.github.com>
2018-12-04 15:16:07 -0600

    Add files via upload

--- Lab8.hs
+++ Lab8.hs
@@ -5,13 +5,14 @@
 -- 1. List Comprehensions 
 
 -- a)
-main = print outfits 
+n = isEven 7
+main = print n 
 a = ["jeans", "khakis"]
 b = ["white shirt", "grey turtleneck", "pink polo", "green hoodie"]
 c = ["brogues", "converse", "sandals"]
 combo x y z = [(x, y, z)| x <- a, y <- b, z <- c] 
 
-outfits = combo a b c
+outf = combo a b c
 
 -- You increase the maximum number of outfits when you either add 
 -- one item to the pants list and one item to the shoes list, or 
@@ -28,12 +29,55 @@ outfits2 = [(x, y, z, w)| x <- a, y <- b, z <- c, w <- d]
 
 -- c)
 p = ["Mr.", "Ms.", "Mrs.", "Dr.", "Prof.", "Rev."]
-honorifics = [h | h <- p] ++ [h ++ " and " ++ g | h <- p, g <-p]
+honor = [h | h <- p] ++ [h ++ " and " ++ g | h <- p, g <-p]
 
 
 -- d)
 bet = ['A'..'Z']
-nato = ["Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliett","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","Xray","Yankee","Zulu"]
-natobet = zip bet nato
+nato1 = ["Alpha","Bravo","Charlie","Delta","Echo","Foxtrot","Golf","Hotel","India","Juliett","Kilo","Lima","Mike","November","Oscar","Papa","Quebec","Romeo","Sierra","Tango","Uniform","Victor","Whiskey","Xray","Yankee","Zulu"]
+natobet = zip bet nato1
 
 translate str = [snd y | x <- str, y <- natobet, x == fst y]
+
+
+-- 2. Functions 
+
+-- a)
+remainder :: Int -> Int -> Int
+remainder j k = j `mod` k 
+
+-- b)
+isEven :: Int -> Bool
+isEven a = if a `mod` 2 == 0
+then True 
+else False 
+
+-- c)
+merge :: [Int] -> [Int] -> [Int]
+merge (x:xs) (y:ys) = if x < y
+then x:(merge xs (y:ys))
+else y:(merge (x:xs) ys)
+merge [] xs = xs
+merge xs [] = xs
+g = [1,5,7,8,13]
+f = [3,9,22]
+r = merge g f
+
+
+-- d)
+removeMultiple:: [Int] -> Int -> [Int]
+removeMultiple lst i = [x | x <- lst, x `mod` i /= 0] 
+
+
+-- 3. List Functions 
+
+-- a) 
+-- what we did for combo in part 1
+outfits x y z = [(x, y, z)| x <- a, y <- b, z <- c] 
+
+-- b)
+honorifics lst = [h | h <- lst] ++ [h ++ " and " ++ g | h <- lst, g <-lst]
+
+-- c)
+nato wdr = [snd y | x <- wdr, y <- natobet, x == fst y]
+
